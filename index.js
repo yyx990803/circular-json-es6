6d2ab4149e769d922a654921f58b95efcf586342
Evan You <yyx990803@gmail.com>
2016-03-08 16:47:29 -0500

    optimize non-circular reference scenarios

--- index.js
+++ index.js
@@ -65,15 +65,21 @@ function isPlainObject (obj) {
 }
 
 exports.stringify = function stringify (data, replacer, space) {
-  var list = []
-  encode(data, replacer, list, new Map())
-  return space
-    ? JSON.stringify(list, null, space) // simply having the space arg makes it slower
-    : JSON.stringify(list)
+  try {
+    return JSON.stringify([data])
+  } catch (e) {
+    var list = []
+    encode(data, replacer, list, new Map())
+    return space
+      ? JSON.stringify(list, null, space) // simply having the space arg makes it slower
+      : JSON.stringify(list)
+  }
 }
 
 exports.parse = function parse (data, reviver) {
   var list = JSON.parse(data)
-  decode(list, reviver)
+  if (list.length > 1) {
+    decode(list, reviver)
+  }
   return list[0]
 }
